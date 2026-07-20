59a38d36dae59301638b830651890fd5ba62363c
Juan Caballero <juandacf@gmail.com>
2026-07-20 18:38:10 -0500

    add new services

--- src/main/java/com/juan/ligaBetPlay/services/PlayerService.java
+++ src/main/java/com/juan/ligaBetPlay/services/PlayerService.java
@@ -1,16 +1,37 @@
 package com.juan.ligaBetPlay.services;
 import com.juan.ligaBetPlay.models.League;
 import com.juan.ligaBetPlay.models.Player;
+import com.juan.ligaBetPlay.models.Team;
 
 public class PlayerService {
 
-    private League league;
 
-    public PlayerService(League league){
-        this.league = league;
-    }
 
-    public void AddNewPlayer(Player player, int teamId ){
+    public boolean AddNewPlayer(League league, int teamId, Player player){
         
+        for(Team team : league.GetLeagueTeams()){
+            if(teamId == team.getTeamId()){
+                team.getAllPlayers().add(player);
+                return true;
+            }
+        }
+
+        return false;
     }
+
+
+    public boolean DeletePlayer(League league, int teamId, int shirtNumber) {
+
+        for(Team team: league.GetLeagueTeams()){
+            if(teamId == team.getTeamId()){
+                for(Player player: team.getAllPlayers() ){
+                    
+                }
+            }
+        }
+
+
+        return false;
+    }
+    
 }
